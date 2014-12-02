eccc42c4e40928da3974983c64f04807bf7bcfa6
András Bucsi <bucsi@beuth-hochschule.de>
2014-12-02 14:09:49 +0100

    email validation

--- js/script.js
+++ js/script.js
@@ -1,38 +1,48 @@
 window.onload = function() {
-    LoginButton.addEventListener('click', validateEmail, true);
-    showFixedScreen();
+    var email_in = document.getElementById('log_email');
+	email_in.addEventListener('keyup', validateEmail, true);
+    if(document.getElementById('start')){
+		showFixedScreen();
+	}
 }
 
 function validateEmail(){
-    if(form.email.value=="") {
-
-
-        setTimeout(function(){alert("Sie haben nichts eingegeben")}, 3440);
-
+	filter = /^([a-zA-Z0-9_\.\-])+\@beuth-hochschule.de/;
+	if (!filter.test(form.email.value)) {
+		document.getElementById('log_email').style.setProperty('box-shadow', '0 0 20px #F00');
     }
-    else if(!form.email.value.endsWith('@beuth-hochschule.de')){
-        //setTimeout(function(){alert("Sie haben nichts eingegeben")}, 5440);
-        form.action="index.html";
-        alert('Für unseren Mitgliederbereich sind nur Angehörige der Beuth-Hochschule mit einer gültigen Beuth Email Adresse zugelassen');
-    }
-    if(form.email.value.endsWith('@beuth-hochschule.de')){
-        //setTimeout(alert('Sie haben sich erfolgreich in unseren Mitgliederbereich eingeloggt'),300);
-       form.action="backend/terminverwaltung.html";
+    else {
+		document.getElementById('log_email').style.setProperty('box-shadow', '0 0 20px #3A2');
+        //setTimeout(function(){form.action="backend/terminverwaltung.html;}",3000);
     }
 }
 
 String.prototype.endsWith = function (s) {
-    return this.length >= s.length && this.substr( this.length - s.length) == s;
+    return this.length >= s.length && this.substr( this.length - s.length+1 ).toLowerCase() == s.toLowerCase();
 };
+
+
 
 function showFixedScreen(){
     bodyTag = document.getElementById('start');
-    bodyTag.style.background = 'url(img/stripes.png), url(img/bmw_ohne.jpg) no-repeat center';
+    bodyTag.style.background = 'url(img/stripes.png), url(img/bmw_riss.jpg) no-repeat center';
     bodyTag.style.backgroundSize = 'cover';
     bodyTag.style.backgroundAttachment = 'fixed';
-    bodyTag.style.transition = 'all 6s ease';
-    /*setTimeout(function(){
+	changeBgImage(bodyTag);
+
+    setTimeout(function(){
                 document.getElementById('welcome').style.display = 'block';
               },4000);
-    setTimeout(function(){document.location="service.html";},12000);*/
+    setTimeout(function(){document.location="service.html";},12000);
+}
+function changeBgImage(element){
+	element.style.setProperty("transition", 		"all 6s ease");
+	element.style.setProperty("-webkit-transition", "all 6s ease");
+	element.style.setProperty("-moz-transition", 	"all 6s ease");
+	element.style.setProperty("-o-transition", 		"all 6s ease");
+	element.style.setProperty("-ms-transition", 	"all 6s ease");
+	element.style.background = 'url(img/stripes.png), url(img/bmw_ohne.jpg) no-repeat center';
+	element.style.backgroundSize = 'cover';
+    element.style.backgroundAttachment = 'fixed';
+	
 }
