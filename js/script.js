9d734a73e1ee0c5f23651725057699ed91c9cef6
András Bucsi <bucsi@beuth-hochschule.de>
2014-12-02 02:40:29 +0100

    changed css, added content and altered javascript

--- js/script.js
+++ js/script.js
@@ -27,16 +27,15 @@ String.prototype.endsWith = function (s) {
 
 function showFixedScreen(){
     bodyTag = document.getElementById('start');
-    bodyTag.style.background = 'url(img/bmw_ohne.jpg) no-repeat center';
-    bodyTag.style.backgroundSize = 'cover';
-    bodyTag.style.backgroundAttachment = 'fixed';
-    bodyTag.style.transition = 'all 4s ease';
-    setTimeout(showStripes(),4000);
-}
-function showStripes() {
-    bodyTag = document.getElementById('start');
     bodyTag.style.background = 'url(img/stripes.png), url(img/bmw_ohne.jpg) no-repeat center';
     bodyTag.style.backgroundSize = 'cover';
     bodyTag.style.backgroundAttachment = 'fixed';
-    bodyTag.style.transition = 'all 4s ease';
+    bodyTag.style.transition = 'all 6s ease';
+    setTimeout(function(){
+                document.getElementById('welcome').style.display = 'block';
+              },4000);
+    setTimeout(function(){document.location="service.html";},12000);
+}
+function showNextPage() {
+
 }
